59c75b62665e36214531609f32dd4dbd28f91753
Deivid de Foggi <deividfoggi@hotmail.com>
2025-03-26 17:34:37 -0300

    creating joke.js and main.js

---
+++ .github/actions/joke-action/main.js
@@ -0,0 +1,11 @@
+const { get } = require("request");
+const joke = require("./joke");
+const core = require("@actions/core");
+
+async function run() {
+    const joke = await getJoke();
+    console.log(joke);
+    core.setOutput("joke-output", joke);
+}
+
+run();
